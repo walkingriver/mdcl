6c5282a7d8f73058ae88f8beaed2e34046145ca0
Michael Callaghan <michael.d.callaghan@disney.com>
2015-03-11 21:42:48 -0400

    Got a single page/route working.

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,35 @@
-// Ionic Starter App
+(function () {
+    angular.module('mdcl', ['ionic'])
+        .controller('SwimController', ['$scope', SwimController])
+        .run(startup)
+        .config(config);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+    function startup ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    }
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    function SwimController($scope) {
+        var vm = this;
+
+        vm.title = 'MDCL';
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('index', {
+                url: '',
+                templateUrl: 'templates/home.html',
+                controller: 'SwimController as vm',
+                controllerAs: 'vm'
+        });
     }
-  });
-})
+})();
