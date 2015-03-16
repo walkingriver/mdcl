d6c14b114728d34a93ac9e128ad6804711dbc947
Michael Callaghan <michael.d.callaghan@disney.com>
2015-03-16 18:53:07 -0400

    Added actionsheet and the ability to mark a workout [in]complete.

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,7 @@
 (function () {
     angular.module('mdcl', ['ionic', 'ngStorage'])
         .controller('SwimController', ['swimServiceFactory', SwimController])
-        .controller('WorkoutController', ['swimServiceFactory', WorkoutController])
+        .controller('WorkoutController', ['$ionicActionSheet', '$state', 'swimServiceFactory', WorkoutController])
         .run(startup)
         .config(config);
 
@@ -12,13 +12,11 @@
             if (window.cordova && window.cordova.plugins.Keyboard) {
                 cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
             }
+            ;
             if (window.StatusBar) {
                 StatusBar.styleDefault();
             }
-            $cordovaActionSheet.show(options)
-                .then(function(btnIndex) {
-                    var index = btnIndex;
-                });
+            ;
         });
     }
 
@@ -28,18 +26,54 @@
         vm.title = 'MDCL';
     }
 
-    function WorkoutController(swimService, $cordovaActionSheet) {
+    function WorkoutController($ionicActionSheet, $state, swimService) {
         var vm = this;
 
         vm.title = 'Workouts';
         vm.workouts = swimService.getWorkout();
         vm.units = swimService.units() || 'yards';
-        vm.numberCompleted = function() {
-            return vm.workouts.filter(function(e){return e.completed}).length;
+        vm.numberCompleted = function () {
+            return vm.workouts.filter(function (e) {
+                return e.completed
+            }).length;
         };
-        vm.dateCompleted = function(workout) {
+        vm.dateCompleted = function (workout) {
             return moment(workout.completed).fromNow();
         };
+        vm.action = action;
+
+        function action(workout) {
+            // Show the action sheet
+            var hideSheet = $ionicActionSheet.show({
+                buttons: [
+                    {text: '<b>Begin Workout</b> '}
+                ],
+                destructiveText: workout.completed ? 'Mark Incomplete' : 'Mark Completed',
+                titleText: 'Week ' + workout.week + '&mdash; Day' + workout.day,
+                cancelText: 'Cancel',
+                cancel: function () {
+                },
+                destructiveButtonClicked: function() {
+                    if (workout.completed)
+                        workout.completed = null;
+                    else
+                        workout.completed = moment();
+
+                    return true;
+                },
+                buttonClicked: function (index) {
+                    switch (index) {
+                        case 0:
+                            $state.go('workout-detail');
+                            break;
+                        default:
+                            break;
+                    }
+
+                    return true;
+                }
+            });
+        }
     }
 
     function config($stateProvider, $urlRouterProvider) {
